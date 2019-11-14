241248e3a4f3696b978be07ed24b7cbe78c77318
joowdx <joowdx@pm.me>
2019-11-15 01:08:33 +0800

    minor progress

--- resources/views/faculties/create.blade.php
+++ resources/views/faculties/create.blade.php
@@ -5,7 +5,59 @@
 @endsection
 
 @section('content')
+{{ $errors }}
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-dark">
+                <div class="card-header">{{ __('Register') }}</div>
 
+                <div class="card-body">
+                    <br>
+                    <form method="post" action="{{ route('faculties.store') }}">z
+                        @csrf
+                        <div class="form-group row">
+                            <label for="uid" class="col-md-4 col-form-label text-md-right">{{ __('UID') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="uid" type="text" class="form-control @error('uid') is-invalid @enderror" name="uid" required autocomplete="new-uid" oninput="this.value=this.value.replace(/[^\d]/,'')" name="uid">
+
+                                @error('uid')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" required autocomplete="new-name" name="name">
+
+                                @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-dark" id="btnsave" style="float:right;">
+                                    {{ __('Create') }}
+                                </button>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
 
 @section('scripts')
--- resources/views/faculties/index.blade.php
+++ resources/views/faculties/index.blade.php
@@ -8,6 +8,7 @@
 
 @section('content')
 <div class="container-fluid">
+    <a href="{{ route('faculties.create') }}" class="nav-link"> Create new </a>
     <div class="row">
         <div class="col-md-12">
             <table id="facultytable" class="table table-bordered" style="cursor:pointer;">
@@ -20,32 +21,32 @@
 
 @section('scripts')
 <script>
-$('#facultytable').DataTable({
+    $('#facultytable').DataTable({
         'destroy': true,
         retrieve: true,
         columnDefs: [],
         "aaSorting": [],
-       'ajax': {
+        'ajax': {
             'url': '{{ url("/api/faculties") }}',
             'type': 'get',
             'dataSrc': e => e,
         },
         'columns': [
-            {
-                'title': 'UID',
-                'data': 'uid',
-            },
-            {
-                'title': 'Name',
-                'data': 'name',
-            },
+        {
+            'title': 'UID',
+            'data': 'uid',
+        },
+        {
+            'title': 'Name',
+            'data': 'name',
+        },
         ],
     });
 
-var table = $('#facultytable').DataTable();
-$('#facultytable tbody').on('click', 'tr', function () {
-    var data = table.row( this ).data();
-    window.location.href="faculties/" + data.id
-} );
+    var table = $('#facultytable').DataTable();
+    $('#facultytable tbody').on('click', 'tr', function () {
+        var data = table.row( this ).data();
+        window.location.href="faculties/" + data.id
+    } );
 </script>
 @endsection
